2ba600af7c15a7bdc4f85d2970f16fd2056efbef
Jorge Oliveira <95076124+jgnunes1@users.noreply.github.com>
2025-02-03 20:52:26 -0300

    primerio

---
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,27 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@Entity
+@Table(name = "pacientes")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+
+    @Embedded
+    private Endereco endereco;
+}
